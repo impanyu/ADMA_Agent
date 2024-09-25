53f927c0ab730d4a26e550db029e7c3c1515a489
impanyu <impanyu@gmail.com>
2024-09-25 12:13:14 -0500

    atest

--- app_new.py
+++ app_new.py
@@ -234,7 +234,7 @@ class meta_program_graph_initializer:
 
 
         response = self.client.beta.chat.completions.parse(
-            model="gpt-4o-mini",
+            model="gpt-4o-2024-08-06",
             messages=[{"role": "system", "content": self.system_prompt},
                       {"role": "user", "content": user_instruction}],
             response_format= initializer_output,
