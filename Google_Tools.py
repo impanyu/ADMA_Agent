7df668e6bc71809f23e442de3152e9c578f337d6
impanyu <impanyu@gmail.com>
2024-10-01 11:23:59 -0500

    test

--- Google_Tools.py
+++ Google_Tools.py
@@ -39,7 +39,8 @@ def google_drive_list(credential_file):
     service = build('drive', 'v3', credentials=credentials)
 
     # Call the Drive v3 API.
-    results = service.files().list(q="'root' in parents",pageSize=100, fields="nextPageToken, files(id, name,size,webViewLink,modifiedTime, createdTime, mimeType)").execute()
+    #results = service.files().list(q="'root' in parents",pageSize=100, fields="nextPageToken, files(id, name,size,webViewLink,modifiedTime, createdTime, mimeType)").execute()
+    results = service.files().list(q="'root' in parents",pageSize=100, fields="nextPageToken, files(name,webViewLink,modifiedTime, createdTime)").execute()
     items = results.get('files', [])
 
     return items
